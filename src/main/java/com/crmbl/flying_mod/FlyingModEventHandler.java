f08bf09c647d63f6ef94e109d511e76f91e5cfb2
CrmbL <axelsch@hotmail.fr>
2020-05-01 22:06:40 +0200

    Fix error on server + free fall damage if wearing item

--- src/main/java/com/crmbl/flying_mod/FlyingModEventHandler.java
+++ src/main/java/com/crmbl/flying_mod/FlyingModEventHandler.java
@@ -10,6 +10,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingFallEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
@@ -18,18 +19,33 @@ public class FlyingModEventHandler {
 
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        if (event.phase == TickEvent.Phase.START /*&& event.side == LogicalSide.SERVER*/) {
+        if (event.phase == TickEvent.Phase.END) {
             PlayerEntity player = event.player;
+            if (player == null)
+                return;
 
-            player.abilities.allowFlying = player.inventory.armorInventory.get(2).getItem() instanceof FlyingModItem;
-            if (player.abilities.allowFlying) {
-                player.abilities.setFlySpeed(0.04f);
-            }
-            else {
-                player.abilities.isFlying = false;
-                player.abilities.setFlySpeed(0.05f);
+            if (event.side == LogicalSide.CLIENT) {
+                player.abilities.allowFlying = player.inventory.armorInventory.get(2).getItem() instanceof FlyingModItem;
+
+                if (player.abilities.allowFlying) {
+                    player.abilities.setFlySpeed(0.04f);
+                }
+                else {
+                    player.abilities.setFlySpeed(0.05f);
+                    player.abilities.isFlying = false;
+                }
             }
         }
+    }
+
+    @SubscribeEvent
+    public void livingFall(LivingFallEvent event) {
+        if (!(event.getEntity() instanceof PlayerEntity))
+            return;
+
+        PlayerEntity eventPlayer = (PlayerEntity)event.getEntity();
+        if (eventPlayer.inventory.armorInventory.get(2).getItem() instanceof FlyingModItem)
+            event.setDistance(0F);
     }
 
     @SubscribeEvent
